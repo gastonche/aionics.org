e1e03f817a8b5f185d39eabea570eebe9851d068
Eugen-Bleck <eugenbleck@gmail.com>
2020-05-28 12:33:40 +0100

    feat: team member image, case-study sections, nav-fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@
         });
         $('#nav-toggle').click(function () {
             $('nav ul').toggle();
+            $('nav .nav-list').addClass('aoinics-mobile')
         });
     });
 })(jQuery);
@@ -33,27 +34,9 @@ function myFunction() {
 
 $(document).ready(function () {
     AOS.init({
-        // Global settings:
-        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-        initClassName: 'aos-init', // class applied after initialization
-        animatedClassName: 'aos-animate', // class applied on animation
-        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-
-        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 120, // offset (in px) from the original trigger point
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 400, // values from 0 to 3000, with step 50ms
-        easing: 'ease', // default easing for AOS animations
-        once: false, // whether animation should happen only once - while scrolling down
-        mirror: false, // whether elements should animate out while scrolling past them
-        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
+        useClassNames: false
     });
-    
+
     // Partners JS
     $(".partners-carousel").owlCarousel({
         loop: true,
@@ -104,17 +87,39 @@ $(document).ready(function () {
     var acc = document.getElementsByClassName("accordion");
     var i;
 
+    $('.vp .accordion').addClass('active');
+    $('.vp .vp-body').addClass('show');
+
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
+            console.log('clicked');
+            $('.vp .vp-body').toggle("hide");
+            if ( $('.vp .accordion').hasClass('active') ) {
+                // $('.vp .vp-body').removeClass('active');
+                $('.vp .vp-body').removeClass('show');
+                console.log('removed active class on click');
+            }
+            if ( $('.vp .vp-body').hasClass('show') ) {
+                // $('.vp .vp-body').removeClass('active');
+                // this.classList.toggle("hide");
+                console.log('removed show class on click');
+            }
+            
             this.classList.toggle("active");
+            // this.classList.toggle("hide");
+            console.log( this );
             var panel = this.nextElementSibling;
             if (panel.style.display === "block") {
                 panel.style.display = "none";
             } else {
                 panel.style.display = "block";
             }
+            
+            
         });
     }
+
+    
 
 });
 
